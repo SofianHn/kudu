5c7235c2cd4b5c68104c1f88539846d63e1e160a
Mingwei Tian <mingwei.tian@microsoft.com>
2014-02-25 16:50:48 -0800

    Activity indicators for buttons.

--- Kudu.Core/SiteExtensions/SiteExtensionManager.cs
+++ Kudu.Core/SiteExtensions/SiteExtensionManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Xml.Linq;
 using Kudu.Contracts.SiteExtensions;
 using Kudu.Contracts.Tracing;
 using Kudu.Core.Infrastructure;
